7bf3b58e229f932bec2df85fc7a0899ab12974fb
Federico <fedefbecerra@gmail.com>
2023-03-08 09:08:27 -0300

    task2 version 2

---
+++ assets/scripts/index.js
@@ -0,0 +1,20 @@
+const divCards = document.getElementById('dCards')
+
+let tarjetas = ''
+
+for (eventos of data.events){
+  tarjetas += `<div class="card m-2" style="width: 18rem;">
+  <img src=${eventos.image} class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title">${eventos.name}</h5>
+    <p class="card-text">${eventos.description}</p>
+    <div class="d-flex flex-wrap justify-content-around align-items-baseline">
+      <p>Pricing: $${eventos.price}</p>
+      <a href="./details.html" class="btn btn-primary">Details</a>
+        </div>
+     </div>
+    </div>
+  </div>`
+}
+   divCards.innerHTML = tarjetas
+
