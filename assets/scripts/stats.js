f8d85f4d02bbfb47cfe7ac9cd362af06a1bf1eb5
Federico <fedefbecerra@gmail.com>
2023-03-22 11:11:14 -0300

    task4 version 4

--- assets/scripts/stats.js
+++ assets/scripts/stats.js
@@ -1,3 +1,7 @@
+const tabla_max_min = document.getElementById('tablaMaxMin')
+const tabla_pasado = document.getElementById('tablaPast')
+const tabla_futuro = document.getElementById('tablaFuture')
+
 
 
 async function fetchData() {
@@ -9,7 +13,104 @@ async function fetchData() {
    return data;
  };
 
- 
-let sData = fetchData()
 
- console.log(sData)
+ async function obtenerArray(){
+            let arrayEventos = await fetchData()
+        
+            porcentajeAsistencia(arrayEventos.events)
+            let eventosFuturos = filtrarEventosFuturos(arrayEventos.events, arrayEventos.currentDate)
+            let eventosPasados = filtrarEventosPasados(arrayEventos.events, arrayEventos.currentDate)
+            let tabla1 = {
+                'mayorCapacidad': eventosPasados.sort(function(a, b){return b.capacity - a.capacity})[0],
+                'mayorPorcentaje': eventosPasados.sort(function(a, b){return b.porcentaje - a.porcentaje})[0],
+                'menorPorcentaje': eventosPasados.sort(function(a, b){return a.porcentaje - b.porcentaje})[0]
+            }
+            
+            let checkboxes = `
+            <tr>
+            <td>${tabla1.mayorPorcentaje.name} (${tabla1.mayorPorcentaje.porcentaje}%)</td>
+            <td>${tabla1.menorPorcentaje.name} (${tabla1.menorPorcentaje.porcentaje}%)</td>
+            <td>${tabla1.mayorCapacidad.name} (${tabla1.mayorCapacidad.capacity})</td>
+            </tr>
+            
+        ` 
+
+        tabla_max_min.innerHTML = checkboxes
+        
+        
+        let categoriasPasadas = fCategorias(eventosPasados)
+        let categoriasFuturas = fCategorias(eventosFuturos)
+    
+        let tablaPasado = obCategory(categoriasPasadas, eventosPasados)
+        let tablaFuturo = obCategory(categoriasFuturas, eventosFuturos)
+    
+    
+    
+        createTable(tablaPasado, tabla_pasado)
+    
+        createTable(tablaFuturo, tabla_futuro)
+
+
+}
+
+
+obtenerArray()
+
+function fCategorias(arr){
+    let categoryArr = Array.from(new Set(arr.map(dataIndex => dataIndex.category)))
+    return categoryArr
+}
+
+
+function porcentajeAsistencia(eventos){
+       eventos.forEach(evento => {
+        evento["porcentaje"] = parseFloat((((evento.estimate || evento.assistance) / evento.capacity * 100)).toFixed(2))
+          });
+    
+}
+
+function filtrarEventosPasados(eventos, fechaActual) {
+    let eventosPasados = eventos.filter((event) => event.date < fechaActual)
+    return eventosPasados
+}
+
+function filtrarEventosFuturos(eventos, fechaActual) {
+    let eventosFuturos = eventos.filter((event) => event.date > fechaActual)
+    return eventosFuturos
+}
+function obCategory(array, objetos){
+    let objetoCategorias = []
+    for (let i = 0; i < array.length; i++) {
+        objetoCategorias.push([])
+        for (const objeto of objetos) {
+            if (array[i] == objeto.category) {
+                objetoCategorias[i].push(objeto)
+            }        
+        }
+    }
+    return objetoCategorias
+ }
+
+ function createTable(array, tabla) {
+    let tablaHTML = ``
+    for (let i = 0; i < array.length; i++) {
+
+        let revenue = 0
+        let porcentajeTabla = 0
+        let categoria
+        for (const evento of array[i]) {
+            categoria = evento.category
+            revenue += evento.price * (evento.estimate || evento.assistance)
+            porcentajeTabla += evento.porcentaje
+        }
+        
+        porcentajeTablaPromedio = porcentajeTabla / (array[i].length)
+        tablaHTML += `
+    <tr>
+        <td>${categoria}</td>
+        <td>${revenue} U$D</td>
+        <td>${parseFloat(porcentajeTablaPromedio.toFixed(2))}%</td>
+    </tr>`
+        tabla.innerHTML = tablaHTML
+    }
+}
